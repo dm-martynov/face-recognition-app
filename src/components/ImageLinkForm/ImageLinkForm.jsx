c468b4cf5000324df320a99b06647537c3cf9084
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-11-18 12:09:33 +0300

    added formik and set up form change

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      urlInput: '',
+    },
+    onSubmit: (values) => {
+      console.log('Form data', values)
+    },
+  })
+
+  console.log('Form values', formik.values)
+
   return (
     <div className='ma4 mt0'>
       <p className='f3'>
         {'This Magic Brain will detect faces in your pictures. Give in a try.'}
       </p>
       <div className='center'>
-        <div className='center form pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>
+        <form
+          onSubmit={formik.handleSubmit}
+          className='center form pa4 br3 shadow-5'
+        >
+          <input
+            className='f4 pa2 w-70 center'
+            type='text'
+            id='urlInput'
+            name='urlInput'
+            onChange={formik.handleChange}
+            value={formik.values.urlInput}
+          />
+          <button
+            type='submit'
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+          >
             Detect
           </button>
-        </div>
+        </form>
       </div>
     </div>
   )
