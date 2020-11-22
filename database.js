99762ba2973be6bf480a01c654eac9ad7f40d15e
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-11-22 12:04:08 +0300

    fixed code to work both im prod and dev

--- database.js
+++ database.js
@@ -1,13 +1,25 @@
 const knex = require('knex')
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+if (process.env.NODE_ENV === 'production') {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  },
-})
+  const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    },
+  })
+} else {
+  const db = knex({
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: '123',
+      database: 'smart-brain',
+    },
+  })
+}
 
 module.exports = db
