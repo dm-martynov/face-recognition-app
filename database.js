9f02c03007dd3fdfd51772feab10903067b54972
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-11-22 12:13:42 +0300

    fixes

--- database.js
+++ database.js
@@ -1,25 +1,23 @@
 const knex = require('knex')
 
-if (process.env.NODE_ENV === 'production') {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
-  const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
-    },
-  })
-} else {
-  const db = knex({
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: '123',
-      database: 'smart-brain',
-    },
-  })
-}
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+})
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'postgres',
+//     password: '123',
+//     database: 'smart-brain',
+//   },
+// })
 
 module.exports = db
