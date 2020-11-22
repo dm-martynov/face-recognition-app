8be68ce593ba850bb83533da43c50d9ded41adf7
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-11-22 12:00:15 +0300

    fixed db to use heroku origin

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
 const knex = require('knex')
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '123',
-    database: 'smart-brain',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   },
 })
 
